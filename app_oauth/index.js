80ac7dfdae95252b575d98ba7934fa5c3d343325
Madyan <madyaneka@protonmail.com>
2021-06-08 08:01:15 +0700

    google authentication implementation

--- app_oauth/index.js
+++ app_oauth/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 const passport = require('passport')
+require('dotenv').config()
 let userProfile
 
 app.use(passport.initialize())
@@ -25,6 +26,27 @@ passport.serializeUser(function(user, cb) {
 passport.deserializeUser(function(obj, cb) {
   cb(null, obj)
 })
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  userProfile = profile
+  return done(null, userProfile)
+}))
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/error'}, (req, res) => {
+  // Successful authentication, redirect success.
+  res.redirect('/success')
+}))
 
 app.get('/', (req, res) => {
   res.render('pages/auth')
