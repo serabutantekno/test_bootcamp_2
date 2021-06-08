a62de66bb8bece5b54c108b650859599e27989f2
Madyan <madyaneka@protonmail.com>
2021-06-08 07:46:24 +0700

    add code related to passport

--- app_oauth/index.js
+++ app_oauth/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const session = require('express-session')
+const passport = require('passport')
+let userProfile
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.set('view engine', 'ejs')
 
@@ -9,6 +14,17 @@ app.use(session({
   saveUninitialized: true,
   secret: 'SECRET'
 }))
+
+app.get('/success', (req, res) => res.send(userProfile))
+app.get('/error', (req, res) => res.send("error logging in"))
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user)
+})
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj)
+})
 
 app.get('/', (req, res) => {
   res.render('pages/auth')
